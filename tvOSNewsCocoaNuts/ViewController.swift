c8d676a24c3eda5d7db4d36d5df5ee2ad0473a7f
Bliss Chapman <bliss@ag-informatics.com>
2016-01-06 19:28:10 -0600

    Initial commit.

--- tvOSNewsCocoaNuts/ViewController.swift
+++ tvOSNewsCocoaNuts/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var headlines = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let news = News()
+        news.fetchTopStories(forSection: News.Section.world) { (fetchResult) -> Void in
+            switch fetchResult {
+            case .Success(let headlines):
+                self.headlines = headlines
+                self.tableView.reloadData()
+            case .Failure(let description):
+                let errorAlert = UIAlertController(title: "Error", message: description, preferredStyle: .Alert)
+                self.presentViewController(errorAlert, animated: true, completion: nil)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return headlines.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("newsCellReuseID")! as UITableViewCell
+        
+        cell.textLabel?.text = headlines[indexPath.row]
+        
+        return cell
+    }
+}
 
+extension ViewController: UITableViewDelegate {
+    
 }
 
